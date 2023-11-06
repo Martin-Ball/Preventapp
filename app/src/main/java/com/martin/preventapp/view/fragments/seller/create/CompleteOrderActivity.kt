83a91492664586d7619961150f85d5d498789045
Martin Ballester <martinballester1@gmail.com>
2023-11-06 12:41:55 -0300

    client selection

--- app/src/main/java/com/martin/preventapp/view/fragments/seller/create/CompleteOrderActivity.kt
+++ app/src/main/java/com/martin/preventapp/view/fragments/seller/create/CompleteOrderActivity.kt
@@ -4,13 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.martin.preventapp.R
 import com.martin.preventapp.controller.seller.createOrder.CreateOrderController
 import com.martin.preventapp.controller.seller.interfaces.CreateOrderInterface
 import com.martin.preventapp.databinding.ActivityClientSelectionBinding
 import com.martin.preventapp.view.activities.seller.MainSellerActivity
+import com.martin.preventapp.view.fragments.seller.orders.DetailOrderFragment
 
-class CompleteOrderActivity : AppCompatActivity() {
+class CompleteOrderActivity : AppCompatActivity(), CreateOrderInterface.CompleteOrderView {
 
     private lateinit var binding: ActivityClientSelectionBinding
 
@@ -19,13 +22,20 @@ class CompleteOrderActivity : AppCompatActivity() {
         binding = ActivityClientSelectionBinding.inflate(layoutInflater);
         setContentView(binding.root)
 
-
+        CreateOrderController.instance!!.setViewClient(this)
     }
 
-    public fun goToMain() {
+    override fun goToMain() {
         val intent = Intent(this, MainSellerActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
         startActivity(intent)
         finish()
+    }
+
+    override fun showFragment(fragment: Fragment) {
+        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        transaction.addToBackStack(null)
+        transaction.replace(R.id.fragment_container, fragment)
+        transaction.commit()
     }
 }
